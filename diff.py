4b5b6758c313b9cd23be8cc6f4386250e24d5d9b
Satvick Acharya <54993534+Satvick16@users.noreply.github.com>
2020-06-23 19:05:18 -0400

    Add files via upload

---
+++ diff.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from math import *
+
+
+def create_slope_field(x_low, x_high, y_low, y_high):
+    fig = plt.figure(figsize=(6, 6))
+    plt.xlim(x_low, x_high)
+    plt.ylim(y_low, y_high)
+
+    ax = plt.gca()
+
+    ax.spines['left'].set_position('zero')
+    ax.spines['right'].set_color('none')
+    ax.spines['bottom'].set_position('zero')
+    ax.spines['top'].set_color('none')
+
+    for x in range(x_low, x_high + 1):
+        for y in range(y_low, y_high + 1):
+            try:
+                m = e ** x - pi ** y
+                # m = x * y
+                # m = x ** y
+                # m = 2 * x / y
+                # m = log(x)
+                # m = log(abs(x))
+                # m = (-1) * x / y
+                reach = 0.3 * cos(atan(m))
+                ax.plot(np.linspace(x - reach, x + reach, 999),
+                        m * (np.linspace(x - reach, x + reach, 999) - x) + y,
+                        c='r', linewidth='1')
+            except ZeroDivisionError:
+                pass
+            except ValueError:
+                pass
+
+    plt.tight_layout()
+    plt.show()
+
+
+def main():
+    x_low = int(input("Please enter the lower x-axis bound: "))
+    x_high = int(input("Please enter the upper x-axis bound: "))
+    y_low = int(input("Please enter the lower y-axis bound: "))
+    y_high = int(input("Please enter the upper y-axis bound: "))
+    create_slope_field(x_low, x_high, y_low, y_high)
+
+
+if __name__ == "__main__":
+    main()
