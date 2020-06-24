e19ec220350401532a9c1e79155bf79673b21960
Satvick Acharya <54993534+Satvick16@users.noreply.github.com>
2020-06-24 12:23:09 -0400

    Update diff.py

--- diff.py
+++ diff.py
@@ -31,17 +31,19 @@ def create_slope_field(x_range, y_range):
                 # m = e ** x - pi ** y
                 # m = x * y
                 # m = x ** y
-                # m = 2 * x / y
+                m = 2 * x / y
                 # m = log(x)
-                m = log(abs(x))
+                # m = log(abs(x))
                 # m = (-1) * x / y
                 reach = 0.3 * cos(atan(m))
                 ax.plot(np.linspace(x - reach, x + reach, 999),
                         m * (np.linspace(x - reach, x + reach, 999) - x) + y,
-                        c='r', linewidth='1')
+                        c='#960a00', linewidth='0.5')
             except ZeroDivisionError:
+                ax.scatter(x, y, s=10, c='#0307fc')
                 pass
             except ValueError:
+                ax.scatter(x, y, s=10, c='#0307fc')
                 pass
 
     plt.tight_layout()
@@ -53,6 +55,10 @@ def main():
     x_range = int(input("Please input the max x value: "))
     y_range = int(input("Please input the max y value: "))
 
+    print()
+    print("Slope lines will be red ticks and points where the slope is undefined will be highlighted in blue.")
+
+    query = bool(input("Do you understand? (press any key): "))
     create_slope_field(x_range, y_range)
 
 
